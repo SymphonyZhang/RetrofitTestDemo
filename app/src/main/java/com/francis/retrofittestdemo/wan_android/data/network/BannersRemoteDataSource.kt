270e62fda4777557b9aa91c5b4df60ff17da47e5
SymphonyZhang <95200261@qq.com>
2025-06-03 15:31:42 +0800

    添加http返回码的error捕获

--- app/src/main/java/com/francis/retrofittestdemo/wan_android/data/network/BannersRemoteDataSource.kt
+++ app/src/main/java/com/francis/retrofittestdemo/wan_android/data/network/BannersRemoteDataSource.kt
@@ -5,6 +5,7 @@ import com.francis.retrofittestdemo.core.domain.util.ResponseResult
 import com.francis.retrofittestdemo.wan_android.data.network.dto.BannerDto
 import kotlinx.coroutines.ensureActive
 import kotlinx.serialization.SerializationException
+import retrofit2.HttpException
 import java.net.UnknownHostException
 import java.nio.channels.UnresolvedAddressException
 import kotlin.coroutines.coroutineContext
@@ -16,7 +17,15 @@ class BannersRemoteDataSource(private val wanAndroidApi: WanAndroidApi) {
     suspend fun getBanners(): ResponseResult<List<BannerDto>, NetworkError> {
         return try {
             ResponseResult.Success(wanAndroidApi.getBanner().data)
-        } catch (e: UnresolvedAddressException) {
+        } catch (e:HttpException){
+            when(e.code()){
+                408 -> ResponseResult.Error(NetworkError.REQUEST_TIMEOUT)
+                429 -> ResponseResult.Error(NetworkError.TOO_MANY_REQUESTS)
+                in 500..599 -> ResponseResult.Error(NetworkError.SERVER_ERROR)
+                else -> ResponseResult.Error(NetworkError.UNKNOWN)
+            }
+        }
+        catch (e: UnresolvedAddressException) {
             ResponseResult.Error(NetworkError.SERVER_ERROR)
         } catch (e: UnknownHostException) {
             ResponseResult.Error(NetworkError.NO_INTERNET)
